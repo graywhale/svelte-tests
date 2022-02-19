2c99bf11526b9fa9d2877ce33721bdbe9f2d9708
ant_bug <ant_bug@sina.com>
2022-02-19 21:09:27 +0800

    Update

--- tests/stories/Button.test.ts
+++ tests/stories/Button.test.ts
@@ -2,17 +2,17 @@ import '@testing-library/jest-dom'
 import {render, fireEvent} from '@testing-library/svelte'
 import Button from "@/stories/Button.svelte";
 
-const label = "Click Me"
+const label = "Button"
 
 test("Check Button Label", () => {
     const {getByText} = render(Button, {label, backgroundColor: 0xffffff})
     expect(getByText(label)).toBeInTheDocument()
 })
-//
-// test('Check Button Click Event', async () => {
-//     const {getByText} = render(Button, {label})
-//     const button = getByText('Button')
-//
-//     await fireEvent.click(button)
-//     expect(button).toHaveTextContent('Button Clicked')
-// })
+
+test('Check Button Click Event', async () => {
+    const {getByText} = render(Button, {label})
+    const button = getByText('Button')
+
+    await fireEvent.click(button)
+    // expect(button).toHaveTextContent('Button Clicked')
+})
